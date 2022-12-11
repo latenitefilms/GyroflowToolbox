427ba06aaab4011e273316ff2cffbf7337f8b1e5
Chris Hocking <latenitefilms@users.noreply.github.com>
2022-12-11 22:01:09 +1100

    Improvements
    - Added some more Rust code. Still works without crashing!

--- Source/Frameworks/gyroflow/src/lib.rs
+++ Source/Frameworks/gyroflow/src/lib.rs
@@ -38,6 +38,39 @@ pub extern "C" fn processFrame(
         
     match manager.import_gyroflow_file(&tempPathSlice, true, |_|(), Arc::new(AtomicBool::new(false))) {
         Ok(_) => {
+            
+            // TODO: Work out what values to put in `set_size`:
+            
+            //manager.set_size(src_rect.2, src_rect.3);
+            
+            manager.set_output_size(1920, 1080);
+
+            {
+                let mut stab = manager.stabilization.write();
+                stab.interpolation = gyroflow_core::stabilization::Interpolation::Lanczos4;
+            }
+
+            manager.invalidate_smoothing();
+            manager.recompute_blocking();
+            manager.params.write().calculate_ramped_timestamps(&manager.keyframes.read());
+            
+            // TODO: Work out the below:
+            
+            /*
+            let mut timestamp_us = 1;
+            
+            let out = stab.process_pixels(timestamp_us, &mut BufferDescription {
+                input_size:  (1920, 1080, stride),
+                output_size: (1920, 1080, output_stride),
+                input_rect: None, // optional
+                output_rect: None, // optional
+                buffers: BufferSource::Cpu {
+                    input:  unsafe { std::slice::from_raw_parts_mut(src_buf.ptr_mut(0), src_buf.bytes()) },
+                    output: unsafe { std::slice::from_raw_parts_mut(dst_buf.ptr_mut(0), dst_buf.bytes()) }
+                }
+            });
+            */
+            
             return 123
         },
         Err(e) => {
