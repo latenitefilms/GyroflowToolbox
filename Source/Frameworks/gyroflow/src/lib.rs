50cc28f93f0d04dc817370c04b0c95a34366d727
Chris Hocking <latenitefilms@users.noreply.github.com>
2022-12-11 21:29:02 +1100

    Improvements
    - Added a build script for the Rust code.
    - We now load the `StabilizationManager`. Baby steps!

--- Source/Frameworks/gyroflow/src/lib.rs
+++ Source/Frameworks/gyroflow/src/lib.rs
@@ -1,3 +1,14 @@
+//
+//  lib.rs
+//  Gyroflow for Final Cut Pro
+//
+//  Created by Chris Hocking on 10/12/2022.
+//
+
+//
+// This code is heavily influenced by: https://github.com/gyroflow/gyroflow-ofx/blob/main/src/fisheyestab_v1.rs
+//
+
 use gyroflow_core::{StabilizationManager, stabilization::RGBAf};
 
 extern crate libc;
@@ -15,5 +26,11 @@ pub extern "C" fn processFrame(
     _buffer_size: u32,
 ) -> i32 {
     println!("[Gyroflow] FROM RUST: processFrame has been triggered!");
-    321
+        
+    let manager = StabilizationManager::<RGBAf>::default();
+    
+    1 // Currently we're just returning one to say "success". Eventually we actually need to return useful data.
 }
+
+
+
