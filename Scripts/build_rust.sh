4564a602d10aab4552f17b69ff97c9c1593bab25
Chris Hocking <latenitefilms@users.noreply.github.com>
2022-12-15 15:45:31 +1100

    We now always use latest gyroflow_core
    - Updated `Cargo.toml` and build script to always use latest `gyroflow_core`.

--- Scripts/build_rust.sh
+++ Scripts/build_rust.sh
@@ -4,6 +4,7 @@ set -eu
 set -o pipefail
 
 cd ~/Documents/GitHub/GyroflowForFinalCutPro/Source/Frameworks/gyroflow
+~/.cargo/bin/cargo update
 ~/.cargo/bin/cargo build --release
 /bin/mv ~/Documents/GitHub/GyroflowForFinalCutPro/Source/Frameworks/gyroflow/target/release/libgyroflow.dylib ~/Documents/GitHub/GyroflowForFinalCutPro/Source/Frameworks/gyroflow/binary/libgyroflow.dylib
 cd ~/Documents/GitHub/GyroflowForFinalCutPro/Source/Frameworks/gyroflow/binary
