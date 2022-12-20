947222b3f60a4d0651ed527b2c053223181e4383
Chris Hocking <latenitefilms@users.noreply.github.com>
2022-12-21 00:48:04 +1100

    Updated Rust Build Script
    - Now builds a universal `dylib` (i.e. both Intel and Apple Silicon).

--- Scripts/build_rust.sh
+++ Scripts/build_rust.sh
@@ -8,7 +8,9 @@ export REPO_HOME ; REPO_HOME="$(greadlink -f "${SCRIPT_HOME}/../")"
 
 cd "${REPO_HOME}/Source/Frameworks/gyroflow"
 ~/.cargo/bin/cargo update
-~/.cargo/bin/cargo build --release
-/bin/mv "${REPO_HOME}/Source/Frameworks/gyroflow/target/release/libgyroflow_toolbox.dylib" "${REPO_HOME}/Source/Frameworks/gyroflow/binary/libgyroflow_toolbox.dylib"
+~/.cargo/bin/cargo build --release --target x86_64-apple-darwin
+~/.cargo/bin/cargo build --release --target aarch64-apple-darwin
+lipo -create -output "${REPO_HOME}/Source/Frameworks/gyroflow/target/libgyroflow_toolbox.dylib" "${REPO_HOME}/Source/Frameworks/gyroflow/target/x86_64-apple-darwin/release/libgyroflow_toolbox.dylib" "${REPO_HOME}/Source/Frameworks/gyroflow/target/aarch64-apple-darwin/release/libgyroflow_toolbox.dylib"
+/bin/mv "${REPO_HOME}/Source/Frameworks/gyroflow/target/libgyroflow_toolbox.dylib" "${REPO_HOME}/Source/Frameworks/gyroflow/binary/libgyroflow_toolbox.dylib"
 cd "${REPO_HOME}/Source/Frameworks/gyroflow/binary"
 /usr/bin/install_name_tool -id "@rpath/libgyroflow_toolbox.dylib" libgyroflow_toolbox.dylib
