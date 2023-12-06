6accffaf064c17c4998e912fab02054916f537cf
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-12-06 13:24:29 +1100

    Added trash_gyroflow_core_fingerprint_and_logs.sh script

---
+++ Scripts/trash_gyroflow_core_fingerprint_and_logs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf /Users/chrishocking/Documents/GitHub/GyroflowToolbox/Source/Frameworks/gyroflow/target/release/.fingerprint/gyroflow* || true
+rm "/Users/chrishocking/Library/Containers/com.latenitefilms.GyroflowToolbox.Renderer/Data/Library/Application Support/FxPlug.log" || true
+rm "/Users/chrishocking/Library/Containers/com.latenitefilms.GyroflowToolbox.Renderer/Data/Library/Application Support/GyroflowCore.log" || true
